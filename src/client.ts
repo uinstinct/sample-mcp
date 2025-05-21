e99c8b5c83a761db92e307cef03d452719c74620
uinstinct <61635505+uinstinct@users.noreply.github.com>
2025-05-21 18:55:56 +0530

    initial commit

---
+++ src/client.ts
@@ -0,0 +1,33 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+async function main() {
+  const transport = new StdioClientTransport({
+    command: "node",
+    args: ["/Users/instinct/Desktop/sample-calc-mcp/build/index.js"],
+    stderr: "pipe",
+  });
+
+  transport.onerror = (error) => {
+    console.log("debug caught", error);
+  };
+
+  const client = new Client({
+    name: "example-client",
+    version: "1.0.0",
+  });
+
+  await client.connect(transport);
+
+  const response = await client.callTool({
+    name: "get-forecast",
+    arguments: {
+      latitude: 0,
+      longitude: 90,
+    },
+  });
+
+  console.log("debug", response);
+}
+
+main();
